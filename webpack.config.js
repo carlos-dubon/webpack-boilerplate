59333cc6273784a1496b7dfea4790a48c8d13221
Carlos Dubón <carlos.dubon@outlook.es>
2020-08-12 15:18:09 -0600

    Init config
    -Added ts locally
    -Init webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
-{
-    
-}
+const path = require("path");
+
+module.exports = {
+  entry: "./src/ts/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
